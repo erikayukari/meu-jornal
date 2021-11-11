e73c26d3a32ecc618ece34ebb6ce56a3709d50c0
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2021-11-11 11:35:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return "<p>Oi!</p>"
+	return """
+	<h1>Home</g1>
+	<a href="#">Home</a>
+	<a href="#sobre">Sobre</a>
+	"""
 
 @app.route("/sobre")
 def sobre():
-	return "<h1>Sobre</h1><p>Este site foi criado por gabriela.</p>"
+	return """
+	<h1>Sobre</h1>
+	<a href="#">Home</a>
+	<a href="#sobre">Sobre</a>
+	<p>Este site foi criado por gabriela.</p>
+	"""
