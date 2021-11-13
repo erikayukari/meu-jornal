7f6ae293f4a9ad24244e03dbbcf051750b088de4
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2021-11-12 21:19:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import requests
+
+def pega_link():
+  url = "https://www.globo.com/"
+  page = requests.get(url)
+  soup = BeautifulSoup(page.content, "html.parser")
+  link_globo = soup.find('a', class_ = 'post__link').attrs['href']
+  return link_globo
 
 app = Flask(__name__)
 
@@ -9,9 +17,10 @@ def hello_world():
 
 @app.route("/sobre")
 def sobre():
-	return """
+	return f"""
 	<h1>Sobre</h1>
 	<a href="/">Home</a>
 	<a href="/sobre">Sobre</a>
+	<p>Este link foi coletado: </br> {link_globo}</p>
 	<p>Este site foi criado por gabriela.</p>
 	"""
