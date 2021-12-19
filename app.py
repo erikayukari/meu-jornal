6924d99e9f64ba7e5d1af29db55601a3a011c3f6
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2021-12-18 21:13:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,6 @@ def manchetes():
 	manchete_estadao = link_estadao()
 	manchete_oglobo = link_oglobo()
 	manchete_metropoles = link_metropoles()
-	manchete_extra = link_extra()
-	manchete_odia = link_odia()
-	manchete_zerohora = link_zerohora()
-	manchete_correio = link_correio()
-	manchete_jc = link_jc()
-	manchete_r7 = link_r7()
-	manchete_opovo = link_opovo()
 	return f"""
 	<h1>Manchetes</h1>
 	<a href="/">Home</a>
@@ -41,13 +34,6 @@ def manchetes():
 	<p>Manchete do Estadão: </br> {manchete_estadao}</p>
 	<p>Manchete d'O Globo: </br> {manchete_oglobo}</p>
 	<p>Manchete do Metrópoles: </br> {manchete_metropoles}</p>
-	<p>Manchete do Extra: </br> {manchete_extra}</p>
-	<p>Manchete d'O Dia: </br> {manchete_odia}</p>
-	<p>Manchete do Zero Hora: </br> {manchete_zerohora}</p>
-	<p>Manchete do Correio Braziliense: </br> {manchete_correio}</p>
-	<p>Manchete do Jornal do Commercio: </br> {manchete_jc}</p>
-	<p>Manchete d'O Povo: </br> {manchete_opovo}</p>
-	<p>Manchete do R7: </br> {manchete_r7}</p>
 	</br>
 	<p>Este site foi criado por Gabriela Caesar.</p>
 	"""
@@ -55,22 +41,6 @@ def manchetes():
 # robo do telegram
 @app.route("/telegram", methods = ["POST"])
 def telegram():
-	# chama funcoes do scraper
-	manchete_globo_com = link_globo_com()
-	manchete_g1 = link_g1()
-	manchete_valor = link_valor()
-	manchete_uol = link_uol()
-	manchete_folha = link_folha()
-	manchete_estadao = link_estadao()
-	manchete_oglobo = link_oglobo()
-	manchete_metropoles = link_metropoles()
-	manchete_extra = link_extra()
-	manchete_odia = link_odia()
-	manchete_zerohora = link_zerohora()
-	manchete_correio = link_correio()
-	manchete_jc = link_jc()
-	manchete_r7 = link_r7()
-	manchete_opovo = link_opovo()
 	# processa mensagem
 	update = request.json
 	chat_id = update["message"]["chat"]["id"]
@@ -83,9 +53,19 @@ def telegram():
 		
 		ATENÇÃO: Digite "Todos" para receber todos os links. Ou digite apenas um nome do veículo por vez. 
 		
-		Opções da imprensa do Brasil: globo.com, g1, Valor, UOL, Folha, Estadão, O Globo, Metrópoles, Extra, O Dia, Zero Hora, Correio Braziliense, R7 e O Povo.
+		Opções da imprensa do Brasil: globo.com, g1, Valor, UOL, Folha, Estadão, O Globo e Metrópoles.
 		"""
 	elif text in ["todos", "todo", "tudo", "todos os links", "todas as manchetes"]:
+		# chama funcoes do scraper
+		manchete_globo_com = link_globo_com()
+		manchete_g1 = link_g1()
+		manchete_valor = link_valor()
+		manchete_uol = link_uol()
+		manchete_folha = link_folha()
+		manchete_estadao = link_estadao()
+		manchete_oglobo = link_oglobo()
+		manchete_metropoles = link_metropoles()
+		# printa mensagem com link
 		answer = f"""
 		segue o link da globo.com: {manchete_globo_com}
 		segue o link do g1: {manchete_g1}
@@ -95,13 +75,6 @@ def telegram():
 		segue o link do Estadão: {manchete_estadao}
 		segue o link d'O Globo: {manchete_oglobo}
 		segue o link do Metrópoles: {manchete_metropoles}
-		segue o link do Extra: {manchete_extra}
-		segue o link d'O Dia: {manchete_odia}
-		segue o link do Zero Hora: {manchete_zerohora}
-		segue o link do Correio Braziliense: {manchete_correio}
-		segue o link do Jornal do Commercio: {manchete_jc}
-		segue o link do R7: {manchete_r7}
-		segue o link d'O Povo: {manchete_opovo}
 		"""
 	elif text in ["globo.com", "globo"]:
 		answer = f"segue o link da globo.com: {manchete_globo_com}"
@@ -119,24 +92,10 @@ def telegram():
 		answer = f"segue o link d'O Globo: {manchete_oglobo}"
 	elif text in ["metropoles", "metropolis"]:
 		answer = f"segue o link do Metrópoles: {manchete_metropoles}"
-	elif text in ["extra", "jornal extra", "o extra"]: 
-		answer = f"segue o link do Extra: {manchete_extra}"
-	elif text in ["o dia", "odia"]: 
-		answer = f"segue o link d'O Dia: {manchete_odia}"
-	elif text in ["zero hora", "zerohora", "0 hora"]: 
-		answer = f"segue o link do Zero Hora: {manchete_zerohora}"
-	elif text in ["correio braziliense", "correio brasiliense"]:
-		answer = f"segue o link do Correio Braziliense: {manchete_correio}"
-	elif text in ["jornal do commercio", "jornal do commércio", "jc", "jornal do comercio", "jornal do comércio"]:
-		answer = f"segue o link do Jornal do Commercio: {manchete_jc}"
-	elif text in ["r7", "r77", "r sete"]: 
-		answer = f"segue o link do R7: {manchete_r7}"
-	elif text in ["o povo", "opovo", "povo"]:
-		answer = f"segue o link d'O Povo: {manchete_opovo}"
 	else:
 		answer = """Por favor, escreva "todos" se desejar receber todos os links. Ou escreva apenas o nome do veículo que te interessa:
 		
-		Opções da imprensa do Brasil: globo.com, g1, Valor, UOL, Folha, Estadão, O Globo, Metrópoles, Extra, O Dia, Zero Hora, Correio Braziliense, R7 e O Povo.
+		Opções da imprensa do Brasil: globo.com, g1, Valor, UOL, Folha, Estadão, O Globo e Metrópoles.
 		"""
 	
 	# responde
